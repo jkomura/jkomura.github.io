0041fe0a58f0afb8f2b6167c25f46422f0682030
jkomura <jun.komura@gmail.com>
2024-10-16 20:17:08 -0400

    Add files via upload

--- siv/js/nav.js
+++ siv/js/nav.js
@@ -1,18 +1,51 @@
+const w = document.documentElement.clientWidth;
 const sections = document.querySelectorAll("section");
 const navLi = document.querySelectorAll("nav ul li");
-window.onscroll = () => {
+const navA = document.querySelectorAll("nav ul li a");
+const aButton = document.querySelectorAll("a.button");
+
+if (w > 430) {
+  window.onscroll = () => {
     var current = "";
-  
     sections.forEach((section) => {
       const sectionTop = section.offsetTop;
       if (scrollY >= sectionTop - 140) {
-        current = section.getAttribute("id"); }
+        current = section.getAttribute("id");
+      }
     });
-  
     navLi.forEach((li) => {
       li.classList.remove("active");
       if (li.classList.contains(current)) {
-        li.classList.add("active");
+        if(!li.classList.contains("active")){
+          li.classList.add("active");
+        }
       }
     });
   };
+} else {
+  navLi.forEach( (li) => {
+    li.classList.remove("active");
+  });
+  navA.forEach( (button) => {
+    button.addEventListener("click", () => {
+      url = button.getAttribute("href");
+      sections.forEach((section) => {
+        section.classList.remove("active");
+        if (section.getAttribute("id") == url.substring(1)) {
+          section.classList.add("active");
+        }
+      });
+    });
+  });
+  aButton.forEach( (button) => {
+    button.addEventListener("click", () => {
+      url = button.getAttribute("href");
+      sections.forEach((section) => {
+        section.classList.remove("active");
+        if (section.getAttribute("id") == url.substring(1)) {
+          section.classList.add("active");
+        }
+      });
+    });
+  });
+}
